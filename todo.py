157539602424ac87b5c1b3d92619bd619a6fa772
Aronk-112 <aronkvaszta12@outlook.com>
2024-01-21 11:28:13 +0100

    Added proper window dimensions

--- todo.py
+++ todo.py
@@ -1,10 +1,14 @@
 import flet as ft
 
 TITLE: str = "Flet Todo App"
+SIZE: tuple = (480, 720)
 
 
 def main(page: ft.Page):
     page.title = TITLE
+    page.window_width = SIZE[0]
+    page.window_height = SIZE[1]
+    page.window_resizable = False
 
     page.add(ft.Text("Hello, world!"))
 
